1ec369ad339460bc55518078aa8940b4744ae157
Toshi Piazza <toshi.piazza@gmail.com>
2015-09-13 10:27:43 -0400

    removes cookie.lua, better conf example

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-REPO_DIR=`pwd`
-
 echo "making build dir"
 mkdir -p /tmp/build-nginx && cd /tmp/build-nginx
 
@@ -9,12 +7,15 @@ echo "getting lua nginx module"
 git clone -q https://github.com/openresty/lua-nginx-module
 echo "getting ngx devel kit"
 git clone -q https://github.com/simpl/ngx_devel_kit
+echo "getting lua resty cookie"
+git clone -q https://github.com/cloudflare/lua-resty-cookie
 echo "getting nginx"
 wget --quiet 'http://nginx.org/download/nginx-1.7.10.tar.gz'
 tar -xzf nginx-1.7.10.tar.gz
 
 DEVEL_MODULE=`pwd`/ngx_devel_kit
 LUA_MODULE=`pwd`/lua-nginx-module
+COOKIE_MODULE=`pwd`/lua-resty-cookie
 
 echo "building nginx & dependencies"
 cd nginx-1.7.10/
@@ -24,5 +25,6 @@ cd nginx-1.7.10/
 make -j > /dev/null
 sudo make install > /dev/null
 
-cd $REPO_DIR
-sh ./mv_config.sh
+# resty is a lua-only library
+cd /tmp/build-nginx
+sudo mv ./lua-resty-cookie/lib/resty/cookie.lua /opt/nginx
